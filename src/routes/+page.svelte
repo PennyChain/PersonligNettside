8e5fb4574e10c32d04fa1361b90135ec42700b1f
Ribe <afs0016376@agderskolen.no>
2024-08-26 00:10:15 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<h1 id="MainHeader">Liam RIBE CV</h1>
+<!-- <p>{a}</p> -->
+
+<script>
+
+    // let a = 100;
+
+</script>
+
+<style>
+
+    #MainHeader {
+        text-align: center;
+    }
+
+</style>
